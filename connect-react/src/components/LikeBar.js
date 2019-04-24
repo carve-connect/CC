cd75c3435b2546aa7ccdf597dc47b0e86b377c2c
RJB <rboucher30@gmail.com>
2019-04-24 10:30:06 -0400

    like dislike on comments work

--- connect-react/src/components/LikeBar.js
+++ connect-react/src/components/LikeBar.js
@@ -12,7 +12,6 @@ export default class LikeBar extends Component {
             likes: [],
             dislikes: [],
             poster: 0,
-            type: [],
             carve: 0, 
             comment: 0,
             media: 0 , 
@@ -37,55 +36,74 @@ export default class LikeBar extends Component {
 
         });
     }
-
     handleLike = e => {
         e.preventDefault();
+        let like = ['like'];
+
+        this.setState  = ({
+            poster: localStorage.getItem('userId'),
+            comment: this.props.comment.comment_id
+        });
 
         axios.post("http://localhost:8000/likes", { 
             poster: localStorage.getItem('userId'),
-            type: ['like'], 
+            likeordislike: like, 
             carve: null, 
             comment: this.props.comment.comment_id,
             media: null,
         })
     }
 
-    clickLike = event => {
-        this.setState = ({
-            type: ['like']
-        })
-    }
 
-    clickDislike = event => {
-        this.setState = ({
-            type: ['dislike']
+    handleDislike= e => {
+        e.preventDefault();
+        let dislike = ['dislike'];
+
+        this.setState  = ({
+            poster: localStorage.getItem('userId'),
+            comment: this.props.comment.comment_id
+        });
+
+        axios.post("http://localhost:8000/likes/", { 
+            poster: localStorage.getItem('userId'),
+            likeordislike: dislike, 
+            carve: null, 
+            comment: this.props.comment.comment_id,
+            media: null,
         })
     }
 
     render() {
         let likeList;
+        let dislikeList;
         let likeCount = 0;
         let dislikeCount = 0 ;
+        let currLike;
 
 
         if(this.state.likes.length > 0){
             likeList = this.state.likes.map((like, index) => {
+                currLike = like;
                 if(like.comment == this.props.comment.comment_id){
-                    if(like.type == "like"){
                         likeCount++;
-                    }
-                    else if(like.type == "dislike"){
-                        dislikeCount++;
-                    }
                     
                 }
             });
         }
+        if(this.state.dislikes.length > 0){
+            dislikeList = this.state.dislikes.map((dislike, index) => {
+                currLike = dislike;
+                if(dislike.comment == this.props.comment.comment_id){
+                    dislikeCount++
+                }
+            });
+        }
+        
         return (
             <>
                 <form >
                     <Button onClick={this.handleLike} variant="link" href="#"><i class="fa fa-thumbs-up fa-10x"></i>{likeCount}</Button>
-                    <Button type="submit" variant="link" href="#"><i class="fa fa-thumbs-down fa-10x"></i>{dislikeCount}</Button>
+                    <Button onClick={this.handleDislike} variant="link" href="#"><i class="fa fa-thumbs-down fa-10x"></i>{dislikeCount}</Button>
                 </form>
             </>
         )
