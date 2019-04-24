1c073aaeffd14793d6e0b40192b24ae5f3a7eb11
RJB <rboucher30@gmail.com>
2019-04-24 09:50:57 -0400

    liking comments post broken

--- connect-react/src/components/LikeBar.js
+++ connect-react/src/components/LikeBar.js
@@ -3,6 +3,7 @@ import {Component} from 'react';
 import axios from 'axios';
 import {Row} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 
 export default class LikeBar extends Component {
     constructor(props){
@@ -10,7 +11,13 @@ export default class LikeBar extends Component {
         this.state = {
             likes: [],
             dislikes: [],
+            poster: 0,
+            type: [],
+            carve: 0, 
+            comment: 0,
+            media: 0 , 
         }
+        this.handleLike=this.handleLike.bind(this);
     }
 
     componentDidMount(){
@@ -31,31 +38,55 @@ export default class LikeBar extends Component {
         });
     }
 
-    printLikes(){
+    handleLike = e => {
+        e.preventDefault();
 
+        axios.post("http://localhost:8000/likes", { 
+            poster: localStorage.getItem('userId'),
+            type: ['like'], 
+            carve: null, 
+            comment: this.props.comment.comment_id,
+            media: null,
+        })
+    }
+
+    clickLike = event => {
+        this.setState = ({
+            type: ['like']
+        })
+    }
+
+    clickDislike = event => {
+        this.setState = ({
+            type: ['dislike']
+        })
     }
 
     render() {
         let likeList;
         let likeCount = 0;
-        let dislikeCount;
+        let dislikeCount = 0 ;
 
 
         if(this.state.likes.length > 0){
             likeList = this.state.likes.map((like, index) => {
                 if(like.comment == this.props.comment.comment_id){
-                    likeCount++;
+                    if(like.type == "like"){
+                        likeCount++;
+                    }
+                    else if(like.type == "dislike"){
+                        dislikeCount++;
+                    }
+                    
                 }
             });
         }
         return (
             <>
-                
-                <a href="#"><i class="fa fa-thumbs-up fa-10x"></i>{likeCount}</a>
-                <br/>
-                <a href="#">Dislike</a>
-        
-            
+                <form >
+                    <Button onClick={this.handleLike} variant="link" href="#"><i class="fa fa-thumbs-up fa-10x"></i>{likeCount}</Button>
+                    <Button type="submit" variant="link" href="#"><i class="fa fa-thumbs-down fa-10x"></i>{dislikeCount}</Button>
+                </form>
             </>
         )
     }
