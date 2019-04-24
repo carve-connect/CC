f4b1db3688a8a5408dc6a30acf1d2977ebd797a9
RJB <rboucher30@gmail.com>
2019-04-24 10:40:09 -0400

    starting create media modal

---
+++ connect-react/src/components/CreateMediaModal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Component} from 'react';
+import {Button} from 'react-bootstrap';
+import {Modal} from 'react-bootstrap';
+
+export default class CreateMediaModal extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+
+    render(){
+
+        return (
+            <>
+                <Button>Hello</Button>
+            </>
+        )
+    }
+
+
+
+}
