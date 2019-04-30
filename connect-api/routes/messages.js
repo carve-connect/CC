837b1ca9e772fb49644e1e02e67fd241a6b3aa08
mrsparkle <mrsparkle1114@gmail.com>
2019-04-29 23:09:33 -0400

    stuff

--- connect-api/routes/messages.js
+++ connect-api/routes/messages.js
@@ -47,7 +47,7 @@ router.post('/reply', (req, res) => {
     // Execute the query to insert into the database
     con.query(new_message, [sender, reciever, subject, body, msgType, carve, type, reply], (err, results) => {
         if (err) throw err;
-        res.status(201).jsonp({results}, recipient).end;
+        res.status(201).jsonp({results}).end;
     })
 });
 
