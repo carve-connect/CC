b85c9545fc518fc53dc28b56f8ff5726a2625329
andersond45 <dylanjanderson4@gmail.com>
2019-05-01 13:16:12 -0400

    Added the new End points we are going to be using

--- connect-api/routes/media.js
+++ connect-api/routes/media.js
@@ -157,5 +157,14 @@ router.delete('/:mediaId', (req,res) => {
     })
 });
 
+router.get('/:like_info', (req, res) => {
+
+    get_media_like_info = "CALL get_media_like_info(?)";
+
+    con.query(get_media_like_info,(err, results) => {
+        if (err) throw err;
+        res.status(200).jsonp({results}).end;
+    })
+});
 
 module.exports = router;
