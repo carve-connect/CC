d49d4d98828a1cc0097c8e598ba24c939ffe94ea
mrsparkle <mrsparkle1114@gmail.com>
2019-05-05 19:11:47 -0400

    middleware/carves/likeFunctions

--- connect-api/routes/CARVES/likesM.js
+++ connect-api/routes/CARVES/likesM.js
@@ -15,36 +15,36 @@ router.get('/', (req,res) => {
 // Grabs all likes from db
 router.get('/dislike', (req,res) => {
     carveId = req.params.carveId;
-
+    functions.carveDislikesGet(carveId, res);
 });
 
 // Creates a new like
 router.post('/', (req,res) => {
     const {poster,carve} = req.body;
-
+    functions.carveLikesPost([poster,carve], res);
 });
 
 // Creates a new like
 router.post('/dislike', (req,res) => {
     const {poster,carve} = req.body;
-
+    functions.carveDislikesPost([poster,carve], res);
 });
 
 // deletes all likes
 router.delete('/', (req,res) => {
-
+    functions.carveLikesDelete(carveId, res);
 });
 
 // Grab specific like by their id
 router.get('/:likeId', (req,res) => {
     const likeId = req.params.likeId;
-
+    functions.carveLikeGet(likeId, res);
 });
 
 // deletes a specific like
 router.delete('/:likeId', (req,res) => {
     const likeId = req.params.likeId;
-
+    functions.carveLikeDelete(likeId, res);
 });
 
 
--- connect-api/routes/middleware/carves/likeFunctions.js
+++ connect-api/routes/middleware/carves/likeFunctions.js
@@ -9,5 +9,55 @@ module.exports = {
             if (err) throw err;
             res.status(200).jsonp({results}).end();
         })
-    }
+    },
+
+    carveDislikesGet: function (carveId, res){
+        like_list = "CALL get_carve_dislikes(?)";
+        con.query(like_list,[carveId], (err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end()
+        })
+    },
+
+    carveLikesPost: function ([poster,carve], res){
+        new_like = "CALL add_like(?,?,?,?,?)";
+        // Execute the query to insert into the database
+        con.query(new_like,[poster,'like',carve,null,null], (err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
+    },
+
+    carveDislikesPost: function ([poster,carve], res){
+        new_like = "CALL add_like(?,?,?,?,?)";
+        // Execute the query to insert into the database
+        con.query(new_like,[poster,'dislike',carve,null,null], (err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
+    },
+
+    carveLikesDelete: function (carveId, res){
+        delete_likes = "CALL delete_likes()";
+        con.query(delete_likes, (err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
+    },
+
+    carveLikeGet: function (likeId, res){
+        get_like  = "call get_like(?)";
+        con.query(get_like, [likeId],(err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end();
+        })
+    },
+
+    carveLikeDelete: function (likeId, res){
+        delete_likes = "CALL delete_like(?)";
+        con.query(delete_likes, [likeId],(err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({msg:'like deleted'}).end();
+        })
+    },
 };
