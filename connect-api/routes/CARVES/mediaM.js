86c065b2f29c0698c91eb05cceb5ef33c55f1ec6
mrsparkle <mrsparkle1114@gmail.com>
2019-05-05 19:27:20 -0400

    middleware/carves/mediaFunctions

--- connect-api/routes/CARVES/mediaM.js
+++ connect-api/routes/CARVES/mediaM.js
@@ -16,48 +16,48 @@ router.get('/', (req,res) => {
 // Creates a new media
 router.post('/', (req,res) => {
     const {poster,url,description,carve,venue,profile} = req.body;
-
+    functions.carveMediaPost([poster,url,description,carve,venue,profile], res);
 });
 
 // updates all medias
 router.put('/', (req,res) => {
-
+    functions.carveAllMediaPut(res);
 });
 
 // updates all medias
 router.patch('/', (req,res) => {
-
+    functions.carveAllMediaPatch(res);
 });
 
 // deletes all medias
 router.delete('/', (req,res) => {
-
+    functions.carveAllMediaDelete(res);
 });
 
 // Grab specific media by their id
 router.get('/:mediaId', (req,res) => {
     const mediaId = req.params.mediaId;
-
+    functions.carveMediaGet(mediaId, res);
 });
 
 // updates a specific media resource
 router.put('/:mediaId', (req,res) => {
     const mediaId = req.params.mediaId;
     const {poster,url,description,carve,venue,profile} = req.body;
-
+    functions.carveMediaPut(mediaId,  [poster,url,description,carve,venue,profile], res);
 });
 
 // updates a specific media resource
 router.patch('/:mediaId', (req,res) => {
     const mediaId = req.params.mediaId;
     const {poster,url,description,carve,venue,profile} = req.body;
-
+    functions.carveMediaPatch(mediaId, [poster,url,description,carve,venue,profile], res);
 });
 
 // deletes a media specific media resource
 router.delete('/:mediaId', (req,res) => {
     const mediaId = req.params.mediaId;
-
+    functions.carveMediaDelete(mediaId, res);
 });
 
 
