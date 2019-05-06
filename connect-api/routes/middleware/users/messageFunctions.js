1b953ac5a7612663a3ba200b8a059dac9602189f
mrsparkle <mrsparkle1114@gmail.com>
2019-05-05 22:53:35 -0400

    middleware/users/messagsFunctions 90% complete

--- connect-api/routes/middleware/users/messageFunctions.js
+++ connect-api/routes/middleware/users/messageFunctions.js
@@ -3,33 +3,85 @@ var router = express.Router({mergeParams: true});
 const con = require('../../../db');
 
 module.exports = {
-    usersMessagesGet: function (res){
-
+    usersMessagesGet: function (userId, res){
+        message_list = "CALL get_users_inbox(?)";
+        con.query(message_list, [userId], (err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end();
+        })
     },
 
-    usersMessagesGet: function (res){
-
+    usersSentGet: function (userId, res){
+        message_list = "CALL get_users_sent(?)";
+        con.query(message_list, [userId], (err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end();
+        })
     },
 
-    usersMessagesGet: function (res){
-
+    usersNotificationsGet: function (userId, res){
+        message_list = "CALL get_user_notifications(?)";
+        con.query(message_list, [userId], (err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end();
+        })
     },
 
-    usersMessagesGet: function (res){
-
+    usersSentNotificaionsGet: function (userId, res){
+        message_list = "CALL get_user_sent_notifications(?)";
+        con.query(message_list, [userId], (err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end();
+        })
     },
 
-    usersMessagesGet: function (res){
-
+    usersMessagePost: function ([sender,reciever,subject,body, msgType], res){
+        new_message = "CALL add_message(?,?,?,?,?)";
+        // Execute the query to insert into the database
+        con.query(new_message,[sender,reciever,subject,body, msgType[0]], (err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
     },
 
-    usersMessagesGet: function (res){
-
+    usersMessagesUpdate: function (res){
+        new_message = "CALL update_messages()";
+        // Execute the query to insert into the database
+        con.query(new_message,(err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
     },
 
-    usersMessagesGet: function (res){
-
+    usersMessagesDelete: function (res){
+        delete_messages = "CALL delete_messages()";
+        con.query(delete_messages, (err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
     },
 
+    usersMessageGet: function (messageId, res){
+        get_message  = "call get_message(?)";
+        con.query(get_message, [messageId],(err, results) => {
+            if (err) throw err;
+            res.status(200).jsonp({results}).end();
+        })
+    },
 
+    usersMessageUpdate: function (messageId, [sender,reciever,subject,body, msgType], res){
+        update_message = "CALL update_message(?,?,?,?,?,?)";
+        con.query(update_message,[messageId,sender,reciever,subject,body, msgType[0]],(err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({results}).end();
+        })
+    },
+
+    usersMessageDelete: function (messageId, res){
+        delete_messages = "CALL delete_message(?)";
+        con.query(delete_messages, [messageId],(err, results) => {
+            if (err) throw err;
+            res.status(201).jsonp({msg:'message deleted'}).end();
+        })
+    }
 };
