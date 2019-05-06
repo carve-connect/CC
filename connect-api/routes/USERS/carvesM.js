8984e6350c3c7c78992ecbd881af90d38f5aac43
mrsparkle <mrsparkle1114@gmail.com>
2019-05-05 20:28:01 -0400

    middleware/users/carveFunctions

--- connect-api/routes/USERS/carvesM.js
+++ connect-api/routes/USERS/carvesM.js
@@ -8,7 +8,7 @@ var functions = require('../middleware/users/carveFunctions');
 router.get('/', (req,res) => {
     // Find all carves from database
     userId = req.params.userId;
-
+    functions.carvesGet(userId,res);
 });
 
 
@@ -16,56 +16,58 @@ router.get('/', (req,res) => {
 router.post('/', (req,res) => {
     creatorId = req.params.userId;
     const {carveName,venueId,carveType,athlete,photo,date, sports} = req.body;
-
+    functions.carvePost(creatorId, [carveName,venueId,carveType,athlete,photo,date, sports], res);
 });
 
 // updates all carves
 router.put('/', (req,res) => {
-
+    functions.carvesUpdate(res);
 });
 
 // updates all carves
 router.patch('/', (req,res) => {
+    functions.carvesUpdate(res);
 
 });
 
 // deletes all carves
 router.delete('/', (req,res) => {
+    functions.carvesDelete(res);
 
 });
 
 // Grab specific carve by their id
 router.get('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
-
+    functions.carveGet(carveId, res);
 });
 
 // updates carve
 router.put('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
     const {carveName,creatorId,venueId,carveType,athlete,photo,date,completed, sports} = req.body;
-
+    functions.carveUpdate(carveId, [carveName,creatorId,venueId,carveType,athlete,photo,date,completed, sports], res);
 });
 
 // updates all carves
 router.patch('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
     const {carveName,creatorId,venueId,carveType,athlete,photo,date,completed, sports} = req.body;
-
+    functions.carveUpdate(carveId, [carveName,creatorId,venueId,carveType,athlete,photo,date,completed, sports], res);
 });
 
 // deletes carve
 router.delete('/:carveId', (req,res) => {
     const carveId = req.params.carveId;
-
+    functions.carveDelete(carveId, res);
 });
 
 router.get('/names', (req,res) => {
-
+    functions.carveGetNames(res);
 });
 
 router.get('/comment_info', (req,res) => {
-
+    functions.carveGetCommentInfo(res);
 });
 
 
